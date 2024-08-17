374a5ee102d1f8951d73dc88ddcbf5f232b7c699
electrolunch <e.lunch@yahoo.com>
2024-08-17 19:32:19 +0300

    1

--- main.py
+++ main.py
@@ -66,6 +66,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = browser_path
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
 # chrome_options.add_argument("--verbose")
 # chrome_options.add_argument("--log-path=cd.log")
 # logging.basicConfig(level=logging.DEBUG)
